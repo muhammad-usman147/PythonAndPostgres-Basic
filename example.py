7c3b8f28c293b6ec7e0bf08e489cb867c2f28e6c
Muhammad Usman Shakeel <ushakeel808@gmail.com>
2021-06-08 19:14:02 +0500

    2nd commit

--- example.py
+++ example.py
@@ -1,19 +1,27 @@
-import psycopg2 as py
+import psycopg2 as pgpy
 
-con = py.connect(
+con = pgpy.connect(
     host = 'localhost',
     database = 'firstdatabase',
     user = 'postgres',
     password = 'usman1234'
 )
-
+#creating cursor
 cur = con.cursor()
 
-cur.execute('Select * from karachi')
+
+#cur.execute("Insert into karachi(id,person, city) values( %s, %s, %s)", (2, 'Saad Alam', 'Ghotki'))
+#con.commit()
+
+#select query
+
+cur.execute("Select * from karachi") #karachi is the table name
 rows = cur.fetchall()
-for r in rows:
-    print(r)
+
+for i in rows:
+    print(i)
+
 
 cur.close()
-con.close()
 
+con.close()
