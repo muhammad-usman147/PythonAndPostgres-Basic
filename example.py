f1c2516764e82681879518117294a4e5777a8dbc
Muhammad Usman Shakeel <ushakeel808@gmail.com>
2021-06-09 20:23:48 +0500

    'update'

--- example.py
+++ example.py
@@ -9,10 +9,15 @@ con = pgpy.connect(
 #creating cursor
 cur = con.cursor()
 
-
+#insert query
 #cur.execute("Insert into karachi(id,person, city) values( %s, %s, %s)", (2, 'Saad Alam', 'Ghotki'))
 #con.commit()
 
+
+#update query
+
+#cur.execute('UPDATE karachi SET person = %s where id = %s',('Faizan Ullah Khan',3))
+#con.commit()
 #select query
 
 cur.execute("Select * from karachi") #karachi is the table name
